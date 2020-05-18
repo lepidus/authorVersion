cb4afaea77ca1b7e5bd0728f5e8c4574a2fabb3e
ada <ada@lepidus.com.br>
2020-05-18 13:47:42 -0400

    Remove opção do usuário autor de publicar a versão nova.
    Signed-off-by: Ada <ada@lepidus.com.br>
    Signed-off-by: Matheus <matheus@lepidus.com.br>

--- VersaoDoAutorPlugin.inc.php
+++ VersaoDoAutorPlugin.inc.php
@@ -21,17 +21,8 @@ class VersaoDoAutorPlugin extends GenericPlugin {
 		if (!Config::getVar('general', 'installed') || defined('RUNNING_UPGRADE')) return true;
 		if ($success && $this->getEnabled($mainContextId)) {
 
-			// Add a new ruleset for publishing
-			\HookRegistry::register('Publication::validatePublish', [$this, 'validate']);
-
-			// Test validation rules in this plugin
-			\HookRegistry::register('Publication::testAuthorValidatePublish', [$this, 'validateTest']);
-
 			// Para sobrescrever um template
 			HookRegistry::register('TemplateResource::getFilename', array($this, '_overridePluginTemplates'));
-
-			// Show plugin rules for editors in settings
-			\HookRegistry::register('Settings::Workflow::listScreeningPlugins', [$this, 'listRules']);
 
 		}
 		return $success;
@@ -63,133 +54,6 @@ class VersaoDoAutorPlugin extends GenericPlugin {
 	 */
 	public function getDescription() {
 		return __('plugins.generic.versaoDoAutor.description');
-	}
-
-
-	/**
-	 * Show plugin rules for editors in settings
-	 *
-	 * @param string $hookName string
-	 * @param array $args
-	 * @return array $rules
-	 */
-	function listRules($hookName, $args) {
-		$rules =& $args[0];
-		$pluginRules['hasPublishedBefore'] = 
-			"<p>" . $this->getDisplayName() . "<br />\n" . 
-			__('plugins.generic.versaoDoAutor.required.publishedBefore') . "</p>\n";
-		$rules = array_merge($rules, $pluginRules);
-		return $rules;
-	}
-
-	/**
-	 * Validate publish, only apply rules to authors
-	 *
-	 * @param string $hookName string
-	 * @param array $args [[
-	 * 	@option array Additional parameters passed with the hook
-	 * 	@option errors array
-	 * 	@option Publication
-	 * ]]
-	 * @return array $errors
-	 */
-	function validate($hookName, $args) {
-		$errors =& $args[0];
-		$publication = $args[1];
-		$currentUser = \Application::get()->getRequest()->getUser();
-		$currentContext = \Application::get()->getRequest()->getContext();
-
-		// Only apply rules to authors, editors can always publish if other criteria is met
-		if ($this->_isAuthor($currentUser->getId(), $publication->getData('submissionId'))){
-
-			$errors = array_merge(
-				$errors,
-				$this->applyRules($currentUser->getId(), $currentContext->getId(), $publication->getData('submissionId'))
-			);
-
-		}
-		return false;
-	}
-
-	/**
-	 * Test validation rules with any user, context and submission
-	 * 
-	 * @param string $hookName string
-	 * @param array $args [[
-	 * 	@option array Additional parameters passed with the hook
-	 * 	@option int $userId
-	 * 	@option int $contextId
-	 * 	@option int $submissionId
-	 * ]]
-	 * @return array $errors
-	 */
-	function validateTest($hookName, $args) {
-		$errors =& $args[0];
-		$userId = $args[1];
-		$contextId = $args[2];
-		$submissionId = $args[3];
-		$errors = array_merge($errors, $this->applyRules($userId, $contextId, $submissionId));
-		return $errors;
-	}
-
-	/**
-	 * Apply rules used in this screening plugin and return errors
-	 * @param int $userId
-	 * @param int $contextId
-	 * @param int $submissionId
-	 * @return array $errors
-	 */
-	function applyRules($userId, $contextId, $submissionId) {
-		$errors = [];
-		// Check that user has published before
-		if (!$this->_hasPublishedBefore($userId, $contextId)) {
-			$errors['hasPublishedBefore'] = __('plugins.generic.versaoDoAutor.required.publishedBefore');
-		}
-		return $errors;
-	}
-
-	//
-	// Custom rules for this screening plugin
-	//
-
-	/**
-	 * Check if user has published before in this context
-	 * @param int $userId
-	 * @param int $contextId
-	 * @return boolean
-	 */
-	function _hasPublishedBefore($userId, $contextId) {
-		$submissionsIterator = Services::get('submission')->getMany([
-			'contextId' => $contextId,
-			'status' => STATUS_PUBLISHED,
-		]);
-		foreach ($submissionsIterator as $submission) {
-			$stageAssignmentDao = DAORegistry::getDAO('StageAssignmentDAO');
-	 		$usersAssignments = $stageAssignmentDao->getBySubmissionAndRoleId($submission->getId(), ROLE_ID_AUTHOR, null, $userId);
-	 		if (!$usersAssignments->wasEmpty()){
-	 			return true;
-	 		}
- 		}
-		return false;
-	}
-
-	//
-	// Helpers
-	//
-
-	/**
-	 * Check if current user is the author of the submission
-	 * @param int $userId
-	 * @param int $submissionId
-	 * @return boolean
-	 */
-	function _isAuthor($userId, $submissionId) {
-		$stageAssignmentDao = DAORegistry::getDAO('StageAssignmentDAO');
-		$usersAssignments = $stageAssignmentDao->getBySubmissionAndRoleId($submissionId, ROLE_ID_AUTHOR, WORKFLOW_STAGE_ID_PRODUCTION, $userId);
-		if (!$usersAssignments->wasEmpty()){
-			return true;
-		}
-		return false;
 	}
 
 }
