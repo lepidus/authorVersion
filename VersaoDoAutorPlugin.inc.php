506e46a1288e0c69f64703656bc6fbdc179149ed
ada <ada@lepidus.com.br>
2020-05-19 16:44:14 -0400

    Adiciona error_log na classe principal para testes.
    Signed-off-by: Ada <ada@lepidus.com.br>
    Signed-off-by: Matheus <matheus@lepidus.com.br>

--- VersaoDoAutorPlugin.inc.php
+++ VersaoDoAutorPlugin.inc.php
@@ -11,10 +11,19 @@
  */
 import('lib.pkp.classes.plugins.GenericPlugin');
 class VersaoDoAutorPlugin extends GenericPlugin {
-
+	public $canPublish = 0;
 	/**
 	 * @copydoc GenericPlugin::register()
 	 */
+	
+	// public function setPermissao($permissao){
+	// 	$this->canPublish = $permissao;
+	// }
+
+	// public function getPermissao(){
+	// 	return $this->canPublish;
+	// }
+
 	public function register($category, $path, $mainContextId = NULL) {
 		$success = parent::register($category, $path, $mainContextId);
 		
@@ -23,7 +32,22 @@ class VersaoDoAutorPlugin extends GenericPlugin {
 
 			// Para sobrescrever um template
 			HookRegistry::register('TemplateResource::getFilename', array($this, '_overridePluginTemplates'));
+			
+			HookRegistry::register('Publication::version', array($this, 'publicar'));
+			HookRegistry::register('Publication::publish::before',  array($this, 'add'));
+			HookRegistry::register('Publication::canAuthorPublish', array($this, 'setAuthorCanPublishVersion'));
+			
+			error_log("canPublish: " . $this->canPublish);
 
+			if($this->canPublish == 1){
+				error_log("com permissão");
+				// HookRegistry::register('Publication::canAuthorPublish', array($this, 'setAuthorCanPublishVersion'));
+			}
+			else{
+				error_log("sem permissão");
+				// HookRegistry::register('Publication::canAuthorPublish',  array($this, 'setAuthorCantPublish'));
+			}
+		
 		}
 		return $success;
 	}
@@ -55,5 +79,47 @@ class VersaoDoAutorPlugin extends GenericPlugin {
 	public function getDescription() {
 		return __('plugins.generic.versaoDoAutor.description');
 	}
+
+	function publicar($hookName, $args) {
+		$this->canPublish = 1;
+		error_log('Permissão:' . $this->canPublish);
+		HookRegistry::call('Publication::canAuthorPublish', array($this));
+		error_log("cria versão");
+		error_log($hookName);
+		return $this;
+	}
+
+	function add($hookName, $args){
+		$this->canPublish = 0;
+		error_log('Permissão:' . $this->canPublish);
+		HookRegistry::call('Publication::canAuthorPublish', array($this));
+		error_log("cria publicação");
+		error_log($hookName);
+		return $this;
+	}
+	/**
+	 * Let authors publish a version when this plugin is enabled
+	 *
+	 * @param string $hookName string
+	 * @param array $args
+	 * @return boolean
+	 */
+	function setAuthorCanPublishVersion($hookName, $args) {
+		error_log('autor pode  publicar');
+		return true;
+	}
+
+		/**
+	 * Let authors publish a publication when this plugin is enabled
+	 *
+	 * @param string $hookName string
+	 * @param array $args
+	 * @return boolean
+	 */
+	function setAuthorCantPublish($hookName, $args) {
+		error_log('autor n pode publicar');
+		return false;
+	}
+
 
 }
