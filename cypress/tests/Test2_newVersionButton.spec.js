4edbe6d6955e85e38f129d6c19ca8d8189e7a775
Jhon <jhon@lepidus.com.br>
2023-08-01 16:17:59 -0400

    Updates integration tests for PKP standard
    Issue: documentacao-e-tarefas/scielo#570
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ cypress/tests/Test2_newVersionButton.spec.js
@@ -0,0 +1,72 @@
+describe('Checks functionality for unpublished history for an author', function () {
+    let submissionData;
+    
+    before(function() {
+        submissionData = {
+            'title': 'Submission for testing Author Version plugin',
+			'abstract': 'Just a simple abstract',
+			'keywords': ['plugin', 'testing']
+		}
+    });
+
+    function step1() {
+        cy.get('input[id^="checklist-"]').click({ multiple: true });
+		cy.get('input[id=privacyConsent]').click();
+		cy.get('button.submitFormButton').click();
+    }
+
+    function step2() {
+        cy.get('#submitStep2Form button.submitFormButton').click();
+    }
+
+    function step3() {
+        cy.get('input[name^="title"]').first().type(submissionData.title, { delay: 0 });
+        cy.get('label').contains('Title').click();
+        cy.get('textarea[id^="abstract-"').then((node) => {
+            cy.setTinyMceContent(node.attr("id"), submissionData.abstract);
+        });
+        cy.get('.section > label:visible').first().click();
+        cy.get('ul[id^="en_US-keywords-"]').then(node => {
+            node.tagit('createTag', submissionData.keywords[0]);
+            node.tagit('createTag', submissionData.keywords[1]);
+        });
+
+        cy.get('#submitStep3Form button.submitFormButton').click();
+    }
+
+    function step4() {
+        cy.waitJQuery();
+		cy.get('#submitStep4Form button.submitFormButton').click();
+		cy.get('button.pkpModalConfirmButton').click();
+    }
+
+    it('Creates new submission as author', function () {
+        cy.login('zwoods', null, 'publicknowledge');
+        cy.get('div#myQueue a:contains("New Submission")').click();
+
+        step1();
+        step2();
+        step3();
+        step4();
+
+        cy.waitJQuery();
+		cy.get('h2:contains("Submission complete")');
+		cy.get('a:contains("Review this submission")').click();
+
+        cy.visit('index.php/publicknowledge/authorDashboard/submission/21');
+        cy.get('button:contains("Post")').should('not.exist');
+        cy.logout();
+    });
+    it('Post submission', function () {
+        cy.findSubmissionAsEditor('dbarnes', null, 'Woods');
+        cy.get('#publication-button').click();
+		cy.get('.pkpHeader .pkpHeader__actions button:contains("Post")').click();
+        cy.get('.pkp_modal_panel button:contains("Post")').click();
+    });
+    it('Check presence of button to create new version for authors', function () {
+        cy.login('zwoods', null, 'publicknowledge');
+        cy.get('archive-button').click();
+        cy.contains('View Woods').click({force: true});
+        cy.get('button:contains("Create New Version")');
+    });
+});
