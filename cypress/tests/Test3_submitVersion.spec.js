fb3f0a4036c57ce9ed051d927ae2f53dcd1aa003
Jhon <jhon@lepidus.com.br>
2023-08-14 16:04:34 -0400

    Adds integration test to check version submission
    Issue: documentacao-e-tarefas/scielo#575
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test3_submitVersion.spec.js
+++ cypress/tests/Test3_submitVersion.spec.js
@@ -35,4 +35,18 @@ describe('Author Version - Submit new version', function () {
         cy.get('h2:contains("Version justification")');
         cy.contains(versionJustification);
     })
+    it('Checks if version submission works if performed just after version creation', function () {
+        cy.login('zwoods', null, 'publicknowledge');
+        cy.get('#archive-button').click();
+        cy.contains('View Woods').click({force: true});
+        cy.get('button:contains("Create New Version")').click();
+        cy.wait(2000);
+
+        cy.get('button:contains("Submit New Version")').click();
+        cy.get('input[name="versionJustification"]').clear().type(versionJustification, {delay: 0});
+        cy.get('div[modalname="submitVersion"] button:contains("Submit")').click();
+        
+        cy.get('h2:contains("Version justification")');
+        cy.contains(versionJustification);
+    });
 });
