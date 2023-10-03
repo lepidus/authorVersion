f0ff5f0d8aa6f351d6400118212933dfc37556d9
Jhon <jhon@lepidus.com.br>
2023-10-03 14:55:31 -0400

    Adds non-submitted filter to submissions database query
    Issue: documentacao-e-tarefas/scielo#596
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- classes/services/queryBuilders/AuthorVersionQueryBuilder.inc.php
+++ classes/services/queryBuilders/AuthorVersionQueryBuilder.inc.php
@@ -5,12 +5,12 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 class AuthorVersionQueryBuilder extends \APP\Services\QueryBuilders\SubmissionQueryBuilder
 {
     protected $newVersion = false;
-    protected $nonSubmitted = false;
+    protected $submittedVersion = false;
 
     public function filterByNewVersion($newVersion, $nonSubmitted)
     {
         $this->newVersion = $newVersion;
-        $this->nonSubmitted = $nonSubmitted;
+        $this->submittedVersion = !$nonSubmitted;
 
         return $this;
     }
@@ -19,14 +19,23 @@ class AuthorVersionQueryBuilder extends \APP\Services\QueryBuilders\SubmissionQu
     {
         if ($this->newVersion) {
             $q->leftJoin('publications as nvp', 'nvp.submission_id', '=', 's.submission_id')
-                ->leftJoin('publication_settings as nvps', 'nvp.publication_id', '=', 'nvps.publication_id')
-                ->where(function ($q) {
-                    $q->where('nvps.setting_name', '=', 'versionJustification');
-                    $q->whereNotNull('nvps.setting_value');
-                });
-            $q->where('nvp.status', '!=', STATUS_PUBLISHED);
+                ->where('nvp.version', '>', 1)
+                ->where('nvp.status', '!=', STATUS_PUBLISHED);
+
+            $submittedVersionSubQuery = function ($query) {
+                $query->select('publication_id')
+                    ->from('publication_settings')
+                    ->where('setting_name', '=', 'versionJustification');
+            };
+
+            if($this->submittedVersion) {
+                $q->whereIn('nvp.publication_id', $submittedVersionSubQuery);
+            } else {
+                $q->whereNotIn('nvp.publication_id', $submittedVersionSubQuery);
+            }
         }
 
         return $q;
     }
+
 }
