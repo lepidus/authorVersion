f3a495fdd2ed21c99720072423817d1a8813184c
Jhon <jhon@lepidus.com.br>
2024-08-12 15:59:33 -0400

    Updates integration test - Relations button rules
    Issue: documentacao-e-tarefas/scielo#691
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test5_relations.spec.js
+++ cypress/tests/Test5_relations.spec.js
@@ -1,7 +1,38 @@
 describe('Author Version - Submission relations updating', function () {
     let publishedArticleDoi = 'https://doi.org/10.1234/Non-existentDoi';
 
-    it('Changes preprint relation to "published as an article"', function () {
+    it('Author can change relations on posted preprint version', function () {
+        cy.login('zwoods', null, 'publicknowledge');
+        cy.get('#myQueue-button').click();
+        cy.contains('View Woods').click({force: true});
+        cy.get('button:contains("Relations")');
+        cy.logout();
+
+        cy.login('dbarnes', null, 'publicknowledge');
+        cy.get('#newVersion-button').click();
+        cy.contains('View Woods').click({force: true});
+        cy.get('#publication-button').click();
+        cy.get('.pkpHeader .pkpHeader__actions button:contains("Post")').click();
+        cy.get('.pkp_modal_panel button:contains("Post")').click();
+        cy.get('.pkpPublication__statusPublished');
+        cy.logout();
+
+        cy.login('zwoods', null, 'publicknowledge');
+        cy.get('#archive-button').click();
+        cy.contains('View Woods').click({force: true});
+        cy.get('.pkpPublication__statusPublished');
+        cy.get('button:contains("Relations")');
+        cy.logout();
+
+        cy.login('dbarnes', null, 'publicknowledge');
+        cy.get('#archive-button').click();
+        cy.contains('View Woods').click({force: true});
+        cy.get('#publication-button').click();
+        cy.get('button:contains("Unpost")').click();
+        cy.get('.modal button:contains("Unpost")').click();
+        cy.get('.pkpPublication__statusUnpublished');
+    });
+    it('Author changes preprint relation to "published as an article"', function () {
         cy.login('zwoods', null, 'publicknowledge');
         cy.get('#myQueue-button').click();
         cy.contains('View Woods').click({force: true});
@@ -22,6 +53,7 @@ describe('Author Version - Submission relations updating', function () {
         cy.get('#publication-button').click();
         cy.get('.pkpHeader .pkpHeader__actions button:contains("Post")').click();
         cy.get('.pkp_modal_panel button:contains("Post")').click();
+        cy.get('.pkpPublication__statusPublished');
     });
     it('Author can not change relations or create new version anymore', function () {
         cy.login('zwoods', null, 'publicknowledge');
