4a2d59e6b46353608ddf3f708fa1c639b174e070
Jhon <jhon@lepidus.com.br>
2025-12-17 15:11:16 -0400

    Email templates updating migration detects locales automatically
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- classes/migrations/UpdateEmailTemplatesMigration.php
+++ classes/migrations/UpdateEmailTemplatesMigration.php
@@ -8,17 +8,34 @@ use APP\facades\Repo;
 
 class UpdateEmailTemplatesMigration extends Migration
 {
-    private const EMAIL_TEMPLATE_LOCALES = ['en', 'es', 'pt_BR'];
-
     public function up(): void
     {
         PluginRegistry::loadCategory('generic');
         $plugin = PluginRegistry::getPlugin('generic', 'authorversionplugin');
+        $emailLocales = $this->getEmailLocales($plugin);
 
         $plugin->addLocaleData();
         Repo::emailTemplate()->dao->installEmailTemplates(
             $plugin->getInstallEmailTemplatesFile(),
-            self::EMAIL_TEMPLATE_LOCALES
+            $emailLocales
         );
+    }
+
+    private function getEmailLocales($plugin): array
+    {
+        $pluginLocalesDirectory = $plugin->getPluginPath() . '/locale/';
+        $emailLocales = [];
+        $localeDirectories = scandir($pluginLocalesDirectory);
+
+        foreach ($localeDirectories as $directory) {
+            if ($directory !== '.' && $directory !== '..' && is_dir($pluginLocalesDirectory . $directory)) {
+                $emailsPoFile = $pluginLocalesDirectory . $directory . '/emails.po';
+                if (file_exists($emailsPoFile)) {
+                    $emailLocales[] = $directory;
+                }
+            }
+        }
+
+        return $emailLocales;
     }
 }
